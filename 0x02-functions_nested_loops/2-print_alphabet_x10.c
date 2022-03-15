d5f3f82965e0ae0ba798f9bd5aa71adc719d4ca2
FrankKamala <frankkamala@gmail.com>
2022-03-16 02:28:15 +0300

    nest for in second for loop

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ *print_alphabet - print the alphabet
+ * Descrition: print the alphabet in lowercase
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	char i;
+    int a;
+for(a = 10; a<= 10; a--; )    {
+for (i = 'a'; i <= 'z'; i++)
+
+
+
+{
+_putchar(i);
+
+}
+}
+_putchar('\n');
+}
