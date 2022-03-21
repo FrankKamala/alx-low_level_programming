d3ce7af41eb2f72917c27445ffa12812945cc8e1
FrankKamala <frankkamala@gmail.com>
2022-03-21 16:01:37 +0300

    swap integers using + and -

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+* swap_int - reset argument value to 98
+* Return: void
+* @a: pointer
+* @b: second pointer
+*/
+void swap_int(int *a,int *b)
+{
+*a=*a+*b;
+*b=*a-*b;
+*a=*a-*b;
+}
