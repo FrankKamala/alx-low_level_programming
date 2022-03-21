2e370c4aa382ce8a8af428aa4dacaeed4fb59bbb
FrankKamala <frankkamala@gmail.com>
2022-03-21 22:56:16 +0300

    loop in array str to print

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <string.h>
+/**
+* _puts - print out string
+* Return: void
+* @str: Pointer s
+*/
+
+void _puts(char *str)
+{
+ printf("%s\n", str);
+}
