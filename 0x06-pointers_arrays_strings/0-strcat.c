f9d26cbbca697cef44e0bcab90a465672d0e47f3
FrankKamala <frankkamala@gmail.com>
2022-03-23 13:15:44 +0300

    file name

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/*
+*
+*
+*
+*
+*
+*/ 
+char *_strcat(char *dest, char *src)
+{
+strcat(char *dest,char *src);
+printf("Concatenated String: %s\n", *dest);
+    
+}
